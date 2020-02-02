2cf02ffeb9a5dd28854b81de4f6ecbbc9d461f3d
klmlboom1013 <klmlboom1013@gmail.com>
2020-02-02 21:57:25 +0900

    컴포넌트 헤더 작성

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,6 +1,8 @@
 <!-- 애플리케이션 이름 표시 -->
 <template>
-    <div>header</div>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
 </template>
 
 <script>
@@ -10,5 +12,9 @@ export default {
 </script>
 
 <style>
-
+    h1 {
+        color: #2F3B52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
 </style>
