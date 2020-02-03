3fdbd4b4a62127196c667de7cb43ef533f5008dd
klmlboom1013 <klmlboom1013@gmail.com>
2020-02-03 18:24:37 +0900

    모달 경고 컴포넌트 추가

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -5,27 +5,43 @@
         <span class="addContainer" v-on:click="addTodo">
             <i class="addBtn fa fa-plus" aria-hidden="true" ></i>
         </span>
+
+        <modal v-if="showModal" @close="showModal=false">
+            <h3 slot="header">경고</h3>
+            <span slot="footer" @click="showModal=false">
+                할 일을 입력하세요.
+                <i class="closeModalBtn fa fa-times" aria-hidden="true"></i>
+            </span>
+        </modal>
+
     </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
 export default {
     data() {
         return {
             newTodoItem: '',
+            showModal: false
         }
     },
     methods: {
         addTodo() {
-            if(this.newTodoItem !== '') {
-                var value = this.newTodoItem && this.newTodoItem.trim();
-                this.$emit('addTodo', value);
-                this.clearInput();
+            if(this.newTodoItem == '') {
+                this.showModal = !this.showModal;
+                return;
             }
+            var value = this.newTodoItem && this.newTodoItem.trim();
+            this.$emit('addTodo', value);
+            this.clearInput();
         },
         clearInput() {
             this.newTodoItem = '';
         }
+    },
+    components: {
+        Modal: Modal
     }
 }
 </script>
